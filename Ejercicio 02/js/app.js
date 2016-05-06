b9285f3624403603ed54831f25c84baf28a449b1
Stephanya <fan.steph@outlook.es>
2016-05-06 13:26:22 -0500

    segunda clase

--- Ejercicio 02/js/app.js
+++ Ejercicio 02/js/app.js
@@ -1,33 +1,35 @@
 
-var app = angular.module('universidadApp', []);
+var app = angular.module('universidadApp',[ ]);
 
 
-app.controller('profesorCtrl', function ($scope) {
+app.controller('profesorCtrl', function($scope){
+		
+	$scope.profesor = profesorData;
+	$scope.editando = {};
 
-		$scope.profesor =profesorData;
-		$scope.editando ={};
-
-
-		$scope.EditarProfesor= function (){
-
-			angular.copy($scope.profesor, $scope.editando);
-		}
-
-		$scope.GuardarCambios= function (){
-
-				angular.copy($scope.editando, $scope.profesor);
+	$scope.mostrarCaja = false;
 
 
-		}
+	$scope.EditarProfesor = function(){
 
-		$scope.CancelarCambios= function (){
+		angular.copy( $scope.profesor, $scope.editando );
+		$scope.mostrarCaja = true;
 
-			$scope.editando = {};
+	}
 
-		}
+	$scope.GuardarCambios = function(){
+		angular.copy( $scope.editando, $scope.profesor );
+		$scope.mostrarCaja = false;
+	}
+
+	$scope.CancelarCambios = function(){
+
+		$scope.editando = {};
+		$scope.mostrarCaja = false;
+
+	}
 
 
-	
 });
 
 
